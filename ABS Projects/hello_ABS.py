8e41ea798acfad9101854fb024fa2327187da5d6
SantyM1442 <santiagomoni98@gmail.com>
2025-01-02 20:25:20 +0100

    Delete ABS Projects directory

--- ABS Projects/hello_ABS.py
+++
@@ -1,9 +0,0 @@
-print('Hello World')
-print('What is your name?') #ask for name
-myName=input()
-print('It is good to meet you', myName)
-print('The length of your name is:', len(myName))
-print('What is your age?') #ask for age
-myAge=input()
-print('You will be '+str(int(myAge)+1)+' in a year.')
-
